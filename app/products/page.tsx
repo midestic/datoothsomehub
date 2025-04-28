0812abbda5b47587fb85fce946269d0206f5c2e4
Usman-Badmus <mideusman209@gmail.com>
2025-04-28 07:55:26 +0100

    product list

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,15 @@
+import { ProductList } from "@/components/ProductList";
+import { stripe } from "@/lib/stripe";
 import React from "react";
 
-export default function ProductsPage() {
-  return <div>productsPage</div>;
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return (
+    <div>
+      <h1>All Products</h1>
+      <ProductList products={products.data} />
+    </div>
+  );
 }
