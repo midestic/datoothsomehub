60bf30e6df536c350adb59354cd960d294f893ca
midestic <badmususman50@gmail.com>
2025-04-30 07:34:53 +0100

    details param

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,15 @@
+import ProductDetail from "@/components/productDetail";
+import { stripe } from "@/lib/stripe";
+import React from "react";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  let product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"],
+  });
+
+  return <ProductDetail product={product} />;
+}
