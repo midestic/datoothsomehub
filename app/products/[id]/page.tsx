79e3dd28dbff4fd11144cd5829916e4e785c2a62
midestic <badmususman50@gmail.com>
2025-04-30 08:13:27 +0100

    paramFix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,9 +5,10 @@ import React from "react";
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const product = await stripe.products.retrieve(params.id, {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
 
