66b0aabda76b0be1ba905ef5c85f53a1c237c4bc
midestic <badmususman50@gmail.com>
2025-05-01 22:59:57 +0100

    cart

--- components/productDetail.tsx
+++ components/productDetail.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import Stripe from "stripe";
 import { Button } from "./ui/button";
+import { useCartStore } from "@/store/cart-store";
 
 interface Props {
   product: Stripe.Product;
 }
 
 export default function ProductDetail({ product }: Props) {
+  const { items, addItem, removeItem } = useCartStore();
   const price = product.default_price as Stripe.Price;
+  const cartItem = items.find((item) => item.id === product.id);
+  const quantity = cartItem ? cartItem.quantity : 0;
+
+  const onAddItem = () => {
+    addItem({
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount as number,
+      imageUrl: product.images ? product.images[0] : null,
+      quantity: 1,
+    });
+  };
+
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
       {product.images && product.images[0] && (
@@ -33,9 +50,11 @@ export default function ProductDetail({ product }: Props) {
           </p>
         )}
         <div className="flex items-center space-x-4">
-          <Button variant="outline">–</Button>
-          <span className="text-lg font-semibold"></span>
-          <Button>+</Button>
+          <Button variant="outline" onClick={() => removeItem(product.id)}>
+            –
+          </Button>
+          <span className="text-lg font-semibold">{quantity}</span>
+          <Button onClick={onAddItem}>+</Button>
         </div>
       </div>
     </div>
